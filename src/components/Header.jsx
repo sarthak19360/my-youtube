8fc7e67f61fdff93f62e98021982bd56ee0f3723
Sarthak <sarthak19360@gmail.com>
2024-02-25 19:55:35 +0530

    Implemented caching in search results using redux store

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,41 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { FaYoutube, FaUserCircle } from "react-icons/fa";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { toggleMenu } from "../utils/appSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { YOUTUBE_SEARCH_API } from "../utils/constant";
 import InputSuggestion from "./InputSuggestion";
+import { cacheResults } from "../utils/searchSlice";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const searchCache = useSelector((store) => store.search);
   const dispath = useDispatch();
   const handleToggleMenu = () => {
     dispath(toggleMenu());
   };
   const fetchSuggestions = async () => {
+    console.log("API Call - " + searchQuery);
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
     setSuggestions(json[1]);
+    dispath(
+      cacheResults({
+        [searchQuery]: json[1],
+      })
+    );
   };
   useEffect(() => {
-    const timer = setTimeout(() => fetchSuggestions(), 200);
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        fetchSuggestions();
+      }
+    }, 200);
     return () => {
       clearTimeout(timer);
     };
@@ -47,16 +62,20 @@ const Header = () => {
             onChange={(e) => {
               setSearchQuery(e.target.value);
             }}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
           />
           <FaMagnifyingGlass className="h-10 w-16 p-3 bg-gray-300 rounded-r-full" />
         </div>
-        <div className="fixed bg-white w-[560px] px-4 py-2 rounded-xl shadow-xl">
-          <ul>
-            {suggestions.map((s) => (
-              <InputSuggestion key={s} text={s} />
-            ))}
-          </ul>
-        </div>
+        {showSuggestions && (
+          <div className="absolute bg-white w-[560px] px-4 py-2 rounded-xl shadow-xl">
+            <ul>
+              {suggestions.map((s) => (
+                <InputSuggestion key={s} text={s} />
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="my-auto">
         <FaUserCircle className="text-2xl" />
